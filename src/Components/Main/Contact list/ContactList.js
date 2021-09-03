67076b2a8aa56092c226584c777cc438ef54d163
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2021-09-03 16:29:24 +0300

    First commit

---
+++ src/Components/Main/Contact list/ContactList.js
@@ -0,0 +1,10 @@
+// Import commponents
+import ContactItem from "./Contact item/ContactItem"
+
+const ContactList = () => {
+    return (
+        <ContactItem />
+    )
+}
+
+export default ContactList
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+// Import css
+import "./index.css";
+
+// Import components
+import SideBar from "./Components/SideBar/SideBar";
+import Main from "./Components/Main/Main";
+
+
+
+const App = () => {
+  return (
+    <div className="container bootstrap snippets bootdeys bootdey">
+      <div className="row decor-default">
+
+        <SideBar />
+
+        <Main />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
