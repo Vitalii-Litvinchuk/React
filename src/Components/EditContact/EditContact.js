5ab44611d09ed23416bf4a77ae235350d01507b8
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2021-09-10 17:45:26 +0300

    Some better code

--- src/Components/EditContact/EditContact.js
+++ src/Components/EditContact/EditContact.js
@@ -5,7 +5,6 @@ class AddContact extends Component {
 
     state =
         {
-            Id: this.props.CurrentContact.Id,
             Name: this.props.CurrentContact.Name,
             Phone: this.props.CurrentContact.Phone,
             Email: this.props.CurrentContact.Email,
@@ -62,10 +61,10 @@ class AddContact extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         const { onEditContact } = this.props;
-        const { Id, Name, Email, Phone, Status, Image, Gender } = this.state;
+        const { Name, Email, Phone, Status, Image, Gender } = this.state;
         const contact =
         {
-            Id,
+            Id: this.props.CurrentContact.Id,
             Name,
             Phone,
             Email,
@@ -84,8 +83,7 @@ class AddContact extends Component {
         if (isRedirect) {
             return <Redirect to="/" />
         }
-        let Gender = this.state.Gender;
-        let Image = this.state.Image;
+        const {Name,Phone,Email,Status,Gender,Image} = this.state
         let img = "";
         if (Image === "" || Image === null)
             img = "https://upload.wikimedia.org/wikipedia/commons/5/50/User_icon-cp.svg"
@@ -104,19 +102,19 @@ class AddContact extends Component {
                         <form className="col-8" onSubmit={this.onSubmit}>
                             <div className="form-group">
                                 <label htmlFor="Name">Name</label>
-                                <input type="text" name="Name" className="form-control" placeholder="Enter name" value={this.state.Name} onChange={this.onGetName} />
+                                <input type="text" name="Name" className="form-control" placeholder="Enter name" value={Name} onChange={this.onGetName} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="Phone">Phone</label>
-                                <input type="tel" name="Phone" className="form-control" placeholder="Enter phone" value={this.state.Phone} onChange={this.onGetPhone} />
+                                <input type="tel" name="Phone" className="form-control" placeholder="Enter phone" value={Phone} onChange={this.onGetPhone} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="Email">Email address</label>
-                                <input type="email" name="Email" className="form-control" placeholder="Enter email" value={this.state.Email} onChange={this.onGetEmail} />
+                                <input type="email" name="Email" className="form-control" placeholder="Enter email" value={Email} onChange={this.onGetEmail} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="Status">Status</label>
-                                <select className="form-select w-100 p-2" value={this.state.Status} onChange={this.onGetStatus}>
+                                <select className="form-select w-100 p-2" value={Status} onChange={this.onGetStatus}>
                                     <option value="Friend">Friend</option>
                                     <option value="Work">Work</option>
                                     <option value="Family">Family</option>
@@ -125,14 +123,14 @@ class AddContact extends Component {
                             </div>
                             <div className="form-group">
                                 <label htmlFor="Gender">Gender</label>
-                                <select className="form-select w-100 p-2" value={this.state.Gender} onChange={this.onGetGender}>
+                                <select className="form-select w-100 p-2" value={Gender} onChange={this.onGetGender}>
                                     <option value="men">Man</option>
                                     <option value="women">Woman</option>
                                 </select>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="Image">Image</label>
-                                <input type="number" name="Image" className="form-control" min="0" max="99" placeholder="Enter number" value={this.state.Image} onChange={this.onGetImage} />
+                                <input type="number" name="Image" className="form-control" min="0" max="99" placeholder="Enter number" value={Image} onChange={this.onGetImage} />
                             </div>
                             <button type="submit" className="btn btn-primary mt-3">Save</button>
                         </form>
